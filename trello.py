06a8e68b12fc702fc745ef228a61ac32077f4ea5
Nikichok <nikichokvu@gmail.com>
2020-07-02 11:36:16 +0400

    Консольный клиент для Trello
    Это мой консольный клиент для Trello!

--- trello.py
+++ trello.py
@@ -14,46 +14,64 @@ board_id = "5ef99e1d61f2ce7c58750c2f"
 
 def read():      
     # Получим данные всех колонок на доске:      
-    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
       
     # Теперь выведем название каждой колонки и всех заданий, которые к ней относятся:      
     for column in column_data:      
-        print(column['name'])    
+        #print(column['name'])    
         # Получим данные всех задач в колонке и перечислим все названия      
         task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
-        print(base_url.format('boards'))
+        print(column['name']+" (задач: " + str(len(task_data)) + ")")
         if not task_data:
           print('\t' + 'Нет задач!')      
           continue      
         for task in task_data:      
           print('\t' + task['name'])  
 
-def create(name, column_name):      
+def create(name, column_name=""):      
     # Получим данные всех колонок на доске      
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()      
-      
-    # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна      
-    for column in column_data:      
-        if column['name'] == column_name:      
-            # Создадим задачу с именем _name_ в найденной колонке      
-            requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})
-            break
+	      
+    # Проверяем, что передан второй парамметр. Если да, то первый - название задачи, которую нужно создать, а второй - название колонки, в которой должна быть создана задача. Если парамметр один, то это название колонки, которую нужно создать.
+    if column_name != "":
+	    # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна      
+	    for column in column_data:      
+	        if column['name'] == column_name:      
+	            # Создадим задачу с именем _name_ в найденной колонке      
+	            requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})
+	            break
+    else:
+      idBoard = column_data[0]['idBoard']
+    	#print(idBoard)
+      requests.post(base_url.format('lists'), data={'name': name, 'idBoard': idBoard, **auth_params})
 
-def move(name, column_name):    
+def move(name, column_name):
     # Получим данные всех колонок на доске    
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()    
         
     # Среди всех колонок нужно найти задачу по имени и получить её id    
-    task_id = None    
+    task_list = []    
     for column in column_data:    
         column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()    
         for task in column_tasks:    
-            if task['name'] == name:    
-                task_id = task['id']    
-                break    
-        if task_id:    
-            break    
-       
+            if task['name'] == name:
+                task_dic = {'id': task['id'], 'name': task['name'], 'name_column': column['name']}    
+                task_list.append(task_dic)    
+    # Если задача не найдена, сообщаем об этом
+    if len(task_list) == 0:
+      print("Задачи " + name + " нет ни в одной колонке")
+      return
+    # Если найдено больше одной задачи, предалаем выбрать, какую перемещать и присваиваем id выбранной задачи переменной task_id
+    elif len(task_list) > 1:
+      print("Задач с наименованием " + name + " несколько:")
+      for i in range(len(task_list)):
+      	print(str(i+1) + ". " + task_list[i]['id'] + ", в колонке - " + task_list[i]['name_column']) 
+      choice = int(input("Выберите какую переместить (введите номер строки):  "))
+      task_id = task_list[choice - 1]['id']
+    
+    elif len(task_list) == 1:
+      task_id = task_list[0]['id']
+
     # Теперь, когда у нас есть id задачи, которую мы хотим переместить    
     # Переберём данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу    
     for column in column_data:    
@@ -65,7 +83,13 @@ def move(name, column_name):
 if __name__ == "__main__":
     if len(sys.argv) <= 2:      
       read()      
-    elif sys.argv[1] == 'create':    
-      create(sys.argv[2], sys.argv[3])    
+    elif sys.argv[1] == 'create' :    
+      if len(sys.argv) <= 3:
+        create(sys.argv[2]) 
+      else:
+        create(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'move':    
       move(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'card':    
+      card(sys.argv[2])
+
